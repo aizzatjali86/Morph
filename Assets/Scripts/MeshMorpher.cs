2ee060e70f096a5d53cea544f4fd5fec50f14b72
aizzatjali86 <aizzat.sazali@gmail.com>
2021-09-19 23:48:01 +0800

    Update MeshMorpher.cs

--- Assets/Scripts/MeshMorpher.cs
+++ Assets/Scripts/MeshMorpher.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+/* Psuedo-code:
+* Correlate old mesh to new mesh
+* - Find bounds and center of model
+* - Get all tris in relation to center, Tc of old and new model
+* - Correspond each Tc to a sphere of average radius (same for both new and old)
+* - Vc is Tc along vector from center to radius
+* - Loop through all Vc of new:
+*      - Find closest to Vc of old
+*      - Get correlation (dict) of new Tc to old Tc
+* 
+* Use Correlation to animate change between old and new
+* - Each Tc old will move to Tc new
+*/
+
+public class MeshMorpher : MonoBehaviour
 {
     public Mesh newMesh;
+
 
 }
